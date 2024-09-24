0b4607290b210d477c4a0568c8ae542a72611a3f
Daniel Roz Arellano <115272571+r0zzz@users.noreply.github.com>
2024-09-24 14:43:28 +0800

    Add files via upload

---
+++ ArellanoAir.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define SIZE 6
+#define LANDINGTIME 6
+#define TAKEOFFTIME 4
+#define TAKEOFFRATE 6
+
+typedef struct {
+    int flightNum[];
+    int front;
+    int rear;
+} TAKEOFF;
+typedef struct {~
+    int flightNum[];
+    int front;
+    int rear;
+} LANDING;
+
+
+int main(void) {
+    return 0;
+}
